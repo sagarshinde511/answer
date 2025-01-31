89a88af2320f12a80d6d23e2505f267bfa0754fa
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-01-31 07:08:10 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -299,7 +299,7 @@ def check_student_login(email, password):
     try:
         db = mysql.connector.connect(host=host, user=user, password=passwd, database=db_name)
         cur = db.cursor()
-        cur.execute("SELECT * FROM studnets WHERE email = %s AND password = %s", (email, password))
+        cur.execute("SELECT * FROM students WHERE email = %s AND password = %s", (email, password))
         return cur.fetchone() is not None
     except Exception as e:
         st.error(f"Database error: {e}")
