a3b12af03b39efbb0a23d4517c89e3c21edb5b9b
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-02-07 00:01:12 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -368,12 +368,12 @@ def process_student_pdf(correct_answers_file, student_pdf):
             # Compute total marks
             total_marks_obtained = df_merged['Assigned Marks'].sum()
             total_possible_marks = correct_answers['Marks'].sum()
-            with open(student_pdf, "rb") as pdf_file:
-                reader = PyPDF2.PdfReader(pdf_file)
-                text = "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
-                subject_name = next((line.split(":")[1].strip() for line in text.split("\n") if "Subject :" in line), "Unknown")
+            #with open(student_pdf, "rb") as pdf_file:
+            #    reader = PyPDF2.PdfReader(pdf_file)
+             #   text = "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
+             #   subject_name = next((line.split(":")[1].strip() for line in text.split("\n") if "Subject :" in line), "Unknown")
 
-            return roll_number, df_merged, total_marks_obtained, total_possible_marks,subject_name
+            return roll_number, df_merged, total_marks_obtained, total_possible_marks,"subject_name"
 
     except Exception as e:
         st.error(f"🚨 Error processing files: {e}")
