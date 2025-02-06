0168dcb651ec234c4624c9c3d28bd5e049c54282
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-02-06 21:24:31 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -663,6 +663,9 @@ def login_page():
 
     with tab3:
         RegisterUser()
+        st.session_state.update({"page": "login", "logged_in": False})
+        st.rerun()
+
     with tab4:
         #st.header("Administrator Login")
         adminLogin()
